cd2d3e551e9651bad55f83306dce4f4c5866ca4c
Edoardo Baldi <edoardob90@gmail.com>
2025-11-04 17:32:10 +0100

    Refactor leaderboard to use a cached and dynamic model

--- backend/storage.py
+++ backend/storage.py
@@ -3,10 +3,11 @@
 Data is ephemeral and exists only during server runtime.
 """
 
-from models import Answer, Participant, Room
+from models import Answer, Leaderboard, Participant, Room
 
 # For now, just dictionaries stored in-memory
 rooms: dict[str, Room] = {}
 participants: dict[str, Participant] = {}
 answers: dict[str, list[Answer]] = {}  # Key is room_id
 active_question_answers: dict[str, list[str]] = {}
+cached_leaderboards: dict[str, Leaderboard] = {}
